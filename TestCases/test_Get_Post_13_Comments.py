d9c98b1a5da9fd5d778540c428575700ef683fad
Saligian <32411071+Saligian@users.noreply.github.com>
2020-02-03 00:23:51 +0100

    Added pytest test files for API
    Test files naming specified. GET tests configured.

---
+++ TestCases/test_Get_Post_13_Comments.py
@@ -0,0 +1,32 @@
+import requests
+import jsonpath
+import config
+
+def test_https_get_post_13_comments():
+    url = config.base_url_https + config.post_id_url
+    response = requests.get(url)
+    json_response = response.json()
+    post_id = jsonpath.jsonpath(json_response[0 - 4], 'postId')
+    comment_id = jsonpath.jsonpath(json_response[0 - 4], 'id')
+    email = jsonpath.jsonpath(json_response[0 - 4], 'email')
+    body = jsonpath.jsonpath(json_response[0 - 4], 'body')
+    assert post_id is not None
+    assert comment_id is not None
+    assert email is not None
+    assert body is not None
+    assert len(json_response) == 5
+
+
+def test_http_get_post_13_comments():
+    url = config.base_url_http + config.post_id_url
+    response = requests.get(url)
+    json_response = response.json()
+    post_id = jsonpath.jsonpath(json_response[0 - 4], 'postId')
+    comment_id = jsonpath.jsonpath(json_response[0 - 4], 'id')
+    email = jsonpath.jsonpath(json_response[0 - 4], 'email')
+    body = jsonpath.jsonpath(json_response[0 - 4], 'body')
+    assert post_id is not None
+    assert comment_id is not None
+    assert email is not None
+    assert body is not None
+    assert len(json_response) == 5
---
+++ config.py
@@ -0,0 +1,3 @@
+base_url_https = 'https://jsonplaceholder.typicode.com/'
+base_url_http = 'http://jsonplaceholder.typicode.com/'
+post_id_url = 'comments?postId='
